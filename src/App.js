4358838f6800b32b130b937df494d71586576f08
dwrik <wrikdas22@gmail.com>
2023-10-28 17:21:10 +0530

    added clipboard functionality, toast notifications

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import DOMPurify from 'dompurify'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { ScrollSync } from 'react-scroll-sync'
+import toast, { Toaster } from 'react-hot-toast'
 import PreviewPane from './components/PreviewPane'
+import DOMPurify from 'dompurify'
 import Editor from './components/Editor'
 import Header from './components/Header'
 import Defaults from './Default'
@@ -10,14 +11,19 @@ const App = () => {
   const [text, setText] = useState(Defaults.TEXT)
   const [safeHTML, setSafeHTML] = useState('')
 
-  const updateText = ({ target }) => {
-    setText(target.value)
-  }
+  const updateText = useCallback((value) => {
+    setText(value)
+  }, [])
 
   useEffect(() => {
     const cleanText = DOMPurify.sanitize(text)
     setSafeHTML(cleanText)
   }, [text])
+
+  const copyMarkdown = () => {
+    navigator.clipboard.writeText(text)
+    toast.success('Copied to clipboard')
+  }
 
   const downloadMarkdown = () => {
     const file = new Blob([text], { type: 'text/plain; charset=utf-8' })
@@ -25,13 +31,23 @@ const App = () => {
     downloadLink.href = URL.createObjectURL(file)
     downloadLink.download = Defaults.FILENAME
     downloadLink.click()
+    toast.success('Download ready')
   }
 
   return (
-    <div className="main">
-      <Header downloadMarkdown={downloadMarkdown} />
+    <div className='main'>
+      <Toaster toastOptions={{
+        iconTheme: {
+          primary: '#F88',
+          secondary: '#fff',
+        },
+      }} />
+      <Header
+        copyMarkdown={copyMarkdown}
+        downloadMarkdown={downloadMarkdown}
+      />
       <ScrollSync>
-        <div className="container">
+        <div className='container'>
           <Editor text={text} updateText={updateText} />
           <PreviewPane safeHTML={safeHTML} />
         </div>
